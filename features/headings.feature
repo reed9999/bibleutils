fcedbd1f921274ef86611ac567953423b60dc2e2
reed9999 <reed9999@uw.edu>
2018-03-11 14:07:20 -0700

    Little bit of test cleanup.
    I concluded that trying to run unit tests from behavioral tests was maybe not a great idea. Have yet to implement the "Retrieve first halves of Matthew, Mark, Luke, John, Acts, and Obadiah" feature.

--- features/headings.feature
+++ features/headings.feature
@@ -1,12 +1,19 @@
 # Created by philip at 3/5/18
-Feature: Parse headings from arbitrary passage
+Feature: Parse headings from arbitrary ESV passage
   We should be able to return the ESV subject headings
 
   Scenario: Retrieve Matthew 5 and parse the Sermon on the Mount
 
   When I ask for the subject headings from Matthew 5
-    Then I should get a sequence containing Sermon on the Mount, the Beatitudes, etc.
+    Then I should get the right headings for Matthew 5
 
-  Scenario: Code base is solid at the unit-test level
 
-    Then Unit tests pass
+  Scenario: Retrieve first halves of Matthew, Mark, Luke, John, Acts, and Obadiah
+
+  When I ask for the subject headings from the first half of Matthew, Mark, Luke, John, Acts, and Obadiah
+    Then I should get the right headings for the first half of Matthew, Mark, Luke, John, Acts, and Obadiah
+
+#  Scenario: Code base is solid at the unit-test level
+#
+#    Then Unit tests pass
+
--- features/steps/headings.py
+++ features/steps/headings.py
@@ -1,5 +1,7 @@
+#import unittest
 from behave import *
 from BibleUtilsApp import BibleUtilsApp
+#from tests.test_bibleUtilsApp import TestBibleUtilsApp
 
 use_step_matcher("re")
 
@@ -15,13 +17,39 @@ def step_impl(context):
     Q_MATTHEW_5 = "Matthew 5"
     context.app.request(Q_MATTHEW_5)
 
-
-
-@then("I should get a sequence containing Sermon on the Mount, the Beatitudes, etc\.")
+@then("I should get the right headings for Matthew 5")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
+    assert context.app is not None
+    print(type(context.app))
 
-    print("The variable value is: " + str(context.app))
+@when("I ask for the subject headings from the first half of Matthew, Mark, Luke, John, Acts, and Obadiah")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
 
+@then("I should get the right headings for the first half of Matthew, Mark, Luke, John, Acts, and Obadiah")
+def step_impl(context):
+    """
+    :type context: behave.runner.Context
+    """
+    pass
+
+# There's probably a good case to be made that mixing unit tests into BDD tests here is incoherent.
+# Anyway, at present pyunit seems to be passing the tests then exiting ungracefully.
+# CF my budget-reed project where this technique seemed to work. Weird.
+
+# @then("Unit tests pass")
+# def step_impl(context):
+#     """
+#     :type context: behave.runner.Context
+#     """
+#     test = TestBibleUtilsApp()
+#     #result = unittest.main(TestBibleUtilsApp)
+#     assert 0 != "Why is trying to execute unittest.main() exiting unceremoniously???"
+#     assert test is not None
+#     assert 'intentional fail' == unittest.main(test)
